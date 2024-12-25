a7d4ceb8e15f792bedefa61b8ef76aead76a3aed
antlogist <antlogist@yandex.ru>
2024-12-25 10:54:04 +0300

    Add form for filtering books by title #9
    Implement a search form to allow users to filter books by title, enhancing usability and simplifying the process of finding specific books.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,6 +3,20 @@
 @section('content')
 <h1 class="mb-10 text-2xl">Books</h1>
 
+<!-- Search by title form -->
+<form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+
+    <!-- Sets the initial value of the input field to the 'title' parameter from the current HTTP request. -->
+    <input type="text" name="title" placeholder="Search by title"
+        value="{{ request('title') }}" class="input h-10" />
+    
+    <button type="submit" class="btn h-10">Search</button>
+    <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
+    
+</form>
+<!-- / Search form -->
+
+<!-- Book list -->
 <ul>
     @forelse ($books as $book)
     <li class="mb-4">
@@ -25,4 +39,5 @@
     </li>
     @endforelse
 </ul>
+<!-- / Book list -->
 @endsection
