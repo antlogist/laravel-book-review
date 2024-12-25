2713ffcf76f84e6237c656f27e8a77ccc93ae268
antlogist <antlogist@yandex.ru>
2024-12-25 10:03:17 +0300

    Add Blade view for books listing #7
    Create a new Blade view to display a list of books, incorporating dynamic data and styling to enhance user experience.

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="mb-10 text-2xl">Books</h1>
+
+<ul>
+    @forelse ($books as $book)
+    <li class="mb-4">
+        <div class="book-item">
+            <div
+                class="flex flex-wrap items-center justify-between">
+                <div class="w-full flex-grow sm:w-auto">
+                    <a href="#" class="book-title">{{ $book->title }}</a>
+                    <span class="book-author">by {{ $book->author }}</span>
+                </div>
+            </div>
+        </div>
+    </li>
+    @empty
+    <li class="mb-4">
+        <div class="empty-book-item">
+            <p class="empty-text">No books found</p>
+            <a href="#" class="reset-link">Reset criteria</a>
+        </div>
+    </li>
+    @endforelse
+</ul>
+@endsection
